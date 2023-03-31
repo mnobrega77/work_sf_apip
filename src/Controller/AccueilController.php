9b40f128a11f407fd676cca0cfd425d0acbf2ca3
@mnobrega <mnobrega77@yahoo.com>
2023-03-31 12:44:45 +0200

    sf

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class AccueilController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
+    #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
         ]);
     }
 }
